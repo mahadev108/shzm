e9bb192adf7092a99f49a2d3a83b2109cd7d69eb
Dmitriy Marсhenkov <demon2565@gmail.com>
2021-10-08 13:08:48 +0300

    Добавил проект

---
+++ shazamMapProject/shazamMapProject/MapScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shazamMapProject
+//
+//  Created by Dmitriy Marchenkov on 08.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
